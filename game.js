554a1e27ab43e5fd32d38b2fddbcf2d974276212
Josh <josh.walters@shaw.ca>
2022-07-19 10:43:28 -0600

    now a playable game!

--- game.js
+++ game.js
@@ -4,38 +4,60 @@ function computerPlay (){
 }
 
 function playGame(playerSelection, computerSelection){
-    console.log("The computer played "+ computerSelection+ " and you have played "+playerSelection)
+    const play = document.getElementById('play')
+    play.style.visibility = 'visible';
+    play.textContent = "The computer played "+ computerSelection+ " and you have played "+playerSelection+"!"
+    const result = document.getElementById('result')
+    result.style.visibility = 'visible';
     if(playerSelection.localeCompare(computerSelection) == 0){
-        console.log("You have tied!")
+        result.textContent = "You have tied!"
         return 0
     }
     else if (playerSelection.localeCompare("rock") == 0){
         if(computerSelection.localeCompare("scissors") == 0){
-            console.log("You win!")
+            result.textContent = "You win!";
             return 1
         }
         else if(computerSelection.localeCompare("paper") == 0){
-            console.log("You lose!")
+            result.textContent = "You lose!";
             return -1
         }
     }
     else{
         if(playerSelection.localeCompare("scissors") == 0 && computerSelection.localeCompare("rock")==0){
-            console.log("You lose!")
+            result.textContent = "You lose!";
             return -1
         }
         else if(playerSelection.length>computerSelection.length){
-            console.log("You win!")
+            result.textContent = "You win!";
             return 1
         }
         else if(playerSelection.length<computerSelection.length){
-            console.log("You lose!")
+            result.textContent = "You lose!"
             return -1
         }
     }
 
 }
-function game(){
+function rockWasClcicked(){
+    playGame('rock',computerPlay());
+}
+function paperWasClcicked(){
+    playGame('paper',computerPlay());
+}
+function scissorsWasClcicked(){
+    playGame('scissors',computerPlay());
+}
+
+let rock = document.querySelector(" .rock");
+rock.addEventListener('click', rockWasClcicked )
+
+let paper = document.querySelector(" .paper");
+paper.addEventListener('click', paperWasClcicked )
+
+let scissors = document.querySelector(" .scissors");
+scissors.addEventListener('click', scissorsWasClcicked )
+/*function game(){
     let score = 0
     for(let i =0; i<5; i++){
     let input = prompt("Enter rock paper or scissors")
@@ -49,7 +71,7 @@ else{console.log("You have lost!")}
 
 let playAgain = true
 
-game()
+game()*/
 
 
 
